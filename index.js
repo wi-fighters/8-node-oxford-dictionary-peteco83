69b150423e92629e7402d74627fb114d33e0dcec
peteco83 <peteco1983@outlook.com>
2020-03-31 10:32:09 +0200

    hiding keys

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+require('dotenv').config()
+
 const axios = require("axios")
 
 const [word] = process.argv.slice(2)
@@ -7,17 +9,16 @@ const [word] = process.argv.slice(2)
 axios(`https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${word}?strictMatch=false`, {
     "method": "GET",
     "headers": {
-        "app_id": "0802b425",
-        "app_key": "1d537d384f8b9c2c3dd49d2aba413b63"
+        "app_id": process.env.API_ID,
+        "app_key": process.env.API_KEY
     }
 
-    }).then(res => {
-        const lexicalEntries = res.data.results[0].lexicalEntries[0]
-        const definitions = lexicalEntries.entries[0].senses
-        console.log(`${word} (${lexicalEntries.lexicalCategory.text})`)
-        definitions.map((def, i) => {
-             console.log(`${i+1}. ${def.definitions}`)
-        })
-        console.log(`Provided by: ${res.data.metadata.provider}`)
-    }).catch(err => console.log(err))
-
+}).then(res => {
+    const lexicalEntries = res.data.results[0].lexicalEntries[0]
+    const definitions = lexicalEntries.entries[0].senses
+    console.log(`${word} (${lexicalEntries.lexicalCategory.text})`)
+    definitions.map((def, i) => {
+        console.log(`${i + 1}. ${def.definitions}`)
+    })
+    console.log(`Provided by: ${res.data.metadata.provider}`)
+}).catch(err => console.log(err))
